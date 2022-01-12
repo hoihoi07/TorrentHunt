3fd7a20173345a31f83fb80111a550de83949793
hoihoi07 <97010331+hoihoi07@users.noreply.github.com>
2022-01-12 17:09:48 +0530

    Update google.py

--- src/inline/google.py
+++ src/inline/google.py
@@ -15,7 +15,7 @@ def googleSuggestions(inline_query, userLanguage):
             bot.answer_inline_query(inline_query.id, queryResult, is_personal=True, switch_pm_text='Google suggestions', switch_pm_parameter='inlineQuery')
             
         else:
-            bot.answer_inline_query(inline_query.id, [telebot.types.InlineQueryResultArticle(id=0, title=language['noResults'][userLanguage], url='https://t.me/h9youtube', hide_url=True, thumb_url='https://raw.githubusercontent.com/hemantapkh/TorrentHunt/main/images/notfound.jpg', input_message_content=telebot.types.InputTextMessageContent(language['noResults'][userLanguage], parse_mode='HTML'))], is_personal=True)
+            bot.answer_inline_query(inline_query.id, [telebot.types.InlineQueryResultArticle(id=0, title=language['noResults'][userLanguage], url='https://t.me/jetbots', hide_url=True, thumb_url='https://raw.githubusercontent.com/hemantapkh/TorrentHunt/main/images/notfound.jpg', input_message_content=telebot.types.InputTextMessageContent(language['noResults'][userLanguage], parse_mode='HTML'))], is_personal=True)
 
     else:
         bot.answer_inline_query(inline_query.id, results=[], cache_time=0, is_personal=True, switch_pm_text=f'Enter the query to search in Google', switch_pm_parameter='inlineQuery')
